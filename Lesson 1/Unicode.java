1cc510d40f3a7af11d96a775611fbd5eb3dc7a85
Yrinka <besederov20@gmail.com>
2020-02-23 14:35:10 +0100

    Master

--- Lesson 1/Unicode.java
+++ Lesson 1/Unicode.java
@@ -1,12 +1,12 @@
 public class Unicode {
     public static void main(String[] args) {
         for (char i = 33; i <= 126; i++) {
-            System.out.println("Unicode: " + i);
+            System.out.println("If krakozyabry are deduced that Unicode: " + i);
         }
 
 
         for (char j = 9398; j <= 10178; j++) {
-            System.out.println("If krakozyabry are deduced that Unicode: " + j);
+            System.out.println("Unicode: " + j);
         }
     }
 }
