d5ad75d8ac06bd8ee9fef41263fdae4a0c9f46f7
Yrinka <besederov20@gmail.com>
2020-02-23 12:05:28 +0100

    My master

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,72 +1,68 @@
 public class Calculator {
     public static void main(String[] args) {
-        int result = 1;
-        for (int i = 0; i <= 13; i++) {
-            result = result + i;
-            if (result % 2 == 0) {
-                System.out.println("Number " + result + " event");
-            } else {
-                System.out.println("Nubber " + result + " no event");
-            }
-        }
-        System.out.println("Sum of numbers: 2 + 6 + 2 + 22 + 20 + 6 + 12 + 8 + 4 + 10 = " + result);
+        int c = 240;
+        int d = 4;
+        char sum = '+';
+        char operation = '+';
+        int sumNumbers = c + d;
+
+        if (operation == sum) {
+            System.out.println("Sum of numbers: 240 + 4 = " + sumNumbers);
+        } else
+            System.out.println(" ");
         System.out.println(" ");
 
-        int result2 = 990;
-        for (int i = 1; i <= 20; i++) {
-            result2 = result2 - i;
-            if (result2 % 2 == 0) {
-                System.out.println("Number " + result2 + " event");
-            } else {
-                System.out.println("Nubber " + result2 + " no event");
-            }
-        }
-        System.out.println("Subtraction of numbers: 990 - 20 - 10 - 14 - 16 - 20 - 10 - 12 - 4 - 104 = " + result2);
+        int m = 580;
+        int l = 6;
+        char subtraction = '-';
+        char operation2 = '-';
+        int subtractionNumbers = m - l;
+        if (operation2 == subtraction) {
+            System.out.println("Subtraction of numbers: 580 - 6 = " + subtractionNumbers);
+        } else
+            System.out.println(" ");
         System.out.println(" ");
 
-        int result3 = 2;
-        for (int i = 2; i <= 5; i++) {
-            result3 = result3 * i;
-            if (result3 % 2 == 0) {
-                System.out.println("Number " + result3 + " event");
-            } else {
-                System.out.println("Nubber " + result3 + " no event");
-            }
-        }
-        System.out.println("Multiplication: 2 * 2 * 6 * 10 = " + result3);
+        int s = 20;
+        int n = 8;
+        char multiplication = '*';
+        char operation3 = '*';
+        int multiplicationNumbers = s * n;
+        if (operation3 == multiplication) {
+            System.out.println("Multiplication of numbers: 20 * 8 = " + multiplicationNumbers);
+        } else
+            System.out.println(" ");
         System.out.println(" ");
 
-        int result4 = 198880;
-        for (int i = 2; i <= 5; i++) {
-            result4 = result4 / i;
-            if (result4 % 2 == 0) {
-                System.out.println("Number " + result4 + " event");
-            } else {
-                System.out.println("Nubber " + result4 + " no event");
-            }
-        }
-        System.out.println("Division of numbers: 198880 / 2 / 3 / 4 / 5 = " + result4);
+        int e = 590;
+        int k = 10;
+        char division = '/';
+        char operation4 = '/';
+        int divisionNumbers = e / k;
+        if (operation4 == division) {
+            System.out.println("Division of numbers: 590 / 10 = " + divisionNumbers);
+        } else
+            System.out.println(" ");
         System.out.println(" ");
 
-        int b = 1;
-        for (int j = 1; j <= 5; j++) b *= 4;
-        {
-            if (b % 2 == 0) {
-                System.out.println("Number " + b + " event");
-            } else {
-                System.out.println("Nubber " + b + " no event");
+        char exponentiation = '^';
+        char operation5 = '^';
+        if (operation5 == exponentiation) {
+            for (int a = 2; a <= 1024; a *= 2) {
+                System.out.println("Raising to the power of the number 2 in the 5th degree = " + a);
             }
-        }
-        System.out.println("Number 2 raised to the power of 5 = " + b);
+        } else
+            System.out.println(" ");
         System.out.println(" ");
 
-        double x = 46894.60;
-        for (int j = 1; j <= 5; j++) {
-            if (x % 2 == 0) {
-                System.out.println("x mod j = " + x % j);
-            } else {
-                System.out.println("x mod j = " + x % j);
-            }
-        }
+        int h = 258;
+        int z = 8;
+        char moduloDivision = '%';
+        char operation7 = '%';
+        double moduloDivisionNumbers = h % z;
+        if (operation7 == moduloDivision) {
+            System.out.println("Division modulo 258 by 8 = " + moduloDivisionNumbers);
+        } else
+            System.out.println(" ");
     }
 }
