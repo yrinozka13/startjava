62d29b2ffbc8bd5d722e29974c37be62df2f908d
Yrinka <besederov20@gmail.com>
2020-02-25 20:09:40 +0100

    First commit

--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -1,16 +1,21 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         int guessedNumber = 48;
-        int guessNumber = 0;
-        do {
-            if (guessNumber < guessedNumber) {
-                System.out.println("The number you entered is less than what the computer made: " + guessNumber);
-                guessNumber++;
-            } else if (guessNumber > guessedNumber) {
-                System.out.println("The number you entered is greater than what the computer made: " + guessNumber);
+        int guessNumber = 7;
+
+        System.out.println("Start!");
+
+        while (guessNumber != guessedNumber) {
+            if (guessNumber > guessedNumber) {
+                System.out.println("You entered the number = " + guessNumber + "The number you entered is greater than what the computer made: ");
                 guessNumber--;
             }
-        } while (guessNumber != guessedNumber);
+            if (guessNumber < guessedNumber) {
+                System.out.println("You entered the number = " + guessNumber + "The number you entered is less than what the computer made: ");
+                guessNumber++;
+            }
+        }
         System.out.println("You guessed! " + guessedNumber);
+        System.out.println("You won! " + guessedNumber);
     }
 }
