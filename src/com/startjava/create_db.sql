5b93a3025f0e7f7ab3c71677d17a9fab314135ea
Yrinka <besederov20@gmail.com>
2020-05-14 23:31:43 +0200

    My first

---
+++ src/com/startjava/create_db.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE jaegers;
+CREATE TABLE models(id SERIAL PRIMARY KEY, modelName TEXT, mark TEXT, height CHAR(6), weight double precision, status TEXT, origin TEXT, launch DATE, kaijuKill INTEGER);
